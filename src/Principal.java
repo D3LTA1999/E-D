baeedfff90485db8776f0874f8eef41927fec5a0
Diego Albor Rojas <daalbor@uninorte.edu.co>
2018-04-28 22:35:08 -0500

    No es que se haya hecho mucho:v version 0.1.3 0:Funcionalidad 1:Codigo 3:Diseño

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,6 @@
 
 import javafx.scene.paint.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /*
@@ -19,6 +20,10 @@ public class Principal extends javax.swing.JFrame {
     public Principal() {
         initComponents();
         this.setLocationRelativeTo(null);
+        resetColor(ingresar);
+        resetColor(registrar);
+        this.setIconImage(new ImageIcon(getClass().getResource("/Imagenes/icons8_Coins_96px_1.png")).getImage());
+        
     }
 
     /**
@@ -35,7 +40,8 @@ public class Principal extends javax.swing.JFrame {
         cerrar = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         Titulo = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        jLabel11 = new javax.swing.JLabel();
+        registrar = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -45,6 +51,9 @@ public class Principal extends javax.swing.JFrame {
         pass_login = new javax.swing.JPasswordField();
         jLabel2 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
+        ingresar = new javax.swing.JPanel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
@@ -65,7 +74,7 @@ public class Principal extends javax.swing.JFrame {
             }
         });
 
-        cerrar.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 36)); // NOI18N
+        cerrar.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 24)); // NOI18N
         cerrar.setForeground(new java.awt.Color(108, 110, 88));
         cerrar.setText("X");
         cerrar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -74,112 +83,176 @@ public class Principal extends javax.swing.JFrame {
             }
         });
 
+        jLabel4.setFont(new java.awt.Font("Arial Rounded MT Bold", 3, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(108, 110, 88));
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("Sistema de valoración de inventario");
 
-        Titulo.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 36)); // NOI18N
-        Titulo.setForeground(new java.awt.Color(62, 66, 58));
-        Titulo.setText("NOMBRE DEL PROGRAMA ");
+        Titulo.setFont(new java.awt.Font("Arial Rounded MT Bold", 3, 36)); // NOI18N
+        Titulo.setForeground(new java.awt.Color(108, 110, 88));
+        Titulo.setText("E&D SYSTEMS");
+
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Coins_96px_1.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(Titulo)
-                        .addGap(28, 28, 28)
-                        .addComponent(cerrar))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addComponent(jLabel4)))
-                .addContainerGap(17, Short.MAX_VALUE))
+                        .addGap(11, 11, 11)
+                        .addComponent(Titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 297, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(cerrar)
+                                .addContainerGap())
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(68, 68, 68))))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addComponent(Titulo))
-                    .addComponent(cerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(32, 32, 32)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(cerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Titulo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addGap(207, 207, 207))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 520, 150));
 
-        jPanel3.addMouseListener(new java.awt.event.MouseAdapter() {
+        registrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                registrarMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                jPanel3MouseEntered(evt);
+                registrarMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                jPanel3MouseExited(evt);
+                registrarMouseExited(evt);
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(108, 110, 88));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Ingresar");
+        jLabel1.setText("Registrarse");
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Registration_30px.png"))); // NOI18N
+
+        javax.swing.GroupLayout registrarLayout = new javax.swing.GroupLayout(registrar);
+        registrar.setLayout(registrarLayout);
+        registrarLayout.setHorizontalGroup(
+            registrarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(registrarLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
-                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(registrarLayout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel8)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+        registrarLayout.setVerticalGroup(
+            registrarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, registrarLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1))
+                .addComponent(jLabel8)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addGap(36, 36, 36))
         );
 
-        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 490, 90, 80));
+        jPanel1.add(registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 440, 90, 80));
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_User_100px_1.png"))); // NOI18N
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 170, 110, 120));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 160, 100, 120));
 
         user_login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 user_loginActionPerformed(evt);
             }
         });
-        jPanel1.add(user_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 320, 190, 30));
+        jPanel1.add(user_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 310, 190, 30));
 
         jLabel3.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(108, 110, 88));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("Usuario");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 320, 110, 30));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 310, 110, 30));
 
         jLabel6.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(108, 110, 88));
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel6.setText("Contraseña");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 380, 120, 30));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 370, 120, 30));
 
-        pass_login.setPreferredSize(new java.awt.Dimension(6, 20));
         pass_login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 pass_loginActionPerformed(evt);
             }
         });
-        jPanel1.add(pass_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 380, 190, 30));
+        jPanel1.add(pass_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 370, 190, 30));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Password_20px_1.png"))); // NOI18N
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 380, 30, 30));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 370, 30, 30));
 
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_User_20px_2.png"))); // NOI18N
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 320, 30, 30));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 310, 30, 30));
+
+        ingresar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                ingresarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                ingresarMouseExited(evt);
+            }
+        });
+
+        jLabel9.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(108, 110, 88));
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("Ingresar");
+
+        jLabel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Enter_30px_1.png"))); // NOI18N
+
+        javax.swing.GroupLayout ingresarLayout = new javax.swing.GroupLayout(ingresar);
+        ingresar.setLayout(ingresarLayout);
+        ingresarLayout.setHorizontalGroup(
+            ingresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ingresarLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(ingresarLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        ingresarLayout.setVerticalGroup(
+            ingresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, ingresarLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel9)
+                .addGap(43, 43, 43))
+        );
+
+        jPanel1.add(ingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 440, 90, 80));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -195,17 +268,16 @@ public class Principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    public void setColor(JPanel panel) {
+        panel.setBackground(new java.awt.Color(197, 197, 197));
+    }
+    
+    public void resetColor(JPanel panel) {
+        panel.setBackground(new java.awt.Color(240, 240, 240));
+    }
     private void cerrarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cerrarMousePressed
         System.exit(0);
     }//GEN-LAST:event_cerrarMousePressed
-
-    private void jPanel3MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseEntered
-        setColor(jPanel3);
-    }//GEN-LAST:event_jPanel3MouseEntered
-
-    private void jPanel3MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseExited
-        resetColor(jPanel3);
-    }//GEN-LAST:event_jPanel3MouseExited
     int xx, xy;
     private void jPanel2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MousePressed
         xx = evt.getX();
@@ -223,14 +295,32 @@ public class Principal extends javax.swing.JFrame {
     private void pass_loginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pass_loginActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_pass_loginActionPerformed
-    public void setColor(JPanel panel) {
-        panel.setBackground(new java.awt.Color(197, 197, 197));
-    }
 
-    public void resetColor(JPanel panel) {
-        panel.setBackground(new java.awt.Color(240, 240, 240));
-    }
+    private void registrarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarMouseExited
+        resetColor(registrar);
+    }//GEN-LAST:event_registrarMouseExited
 
+    private void registrarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarMouseEntered
+        setColor(registrar);
+    }//GEN-LAST:event_registrarMouseEntered
+
+    private void ingresarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ingresarMouseEntered
+        setColor(ingresar);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ingresarMouseEntered
+
+    private void ingresarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ingresarMouseExited
+        resetColor(ingresar);
+
+    }//GEN-LAST:event_ingresarMouseExited
+
+    private void registrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarMouseClicked
+        Registro rg = new Registro();
+        rg.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_registrarMouseClicked
+
+    
     /**
      * @param args the command line arguments
      */
@@ -269,7 +359,10 @@ public class Principal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Titulo;
     private javax.swing.JLabel cerrar;
+    private javax.swing.JPanel ingresar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -277,10 +370,11 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JPasswordField pass_login;
+    private javax.swing.JPanel registrar;
     private javax.swing.JTextField user_login;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/Usuario.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+import java.util.Random;
+
+/**
+ *
+ * @author daalb
+ */
+public class Usuario {
+
+    private String nombres;
+    private String apellidos;
+    private String cedula;
+    private String pass;
+    private String tipo;
+    private String user;
+    FileWriter fr;
+    PrintWriter pw;
+
+    public Usuario() {
+        nombres = null;
+        apellidos = null;
+        cedula = null;
+        pass = null;
+        tipo = null;
+        user = null;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String crearUser(String nombres, String apellidos) {
+        String n = "", a = "", user = "";
+        int j = 0;
+        boolean sw = true;
+        for (int i = 0; i < 1; i++) {
+            n = n + nombres.substring(0, i + 1);
+        }
+        while (j < apellidos.length() && sw == true) {
+            if (apellidos.substring(0, j).equals(" ")) {
+                sw = false;
+            } else {
+                a = a + apellidos.substring(0, j + 1);
+            }
+            j++;
+        }
+        user = n + a;
+        return user;
+    }
+
+    public String crearPass() {
+        int contra;
+        contra = (int) (Math.random() * 9999) + 1000;
+        return String.valueOf(contra);
+    }
+
+    public void llenado(String nombres, String apellidos, String cedula, String pass, String tipo, String user) throws IOException {//Metodo para llenar
+        File users = new File("usuarios.txt");
+        try {
+            FileWriter fw = new FileWriter(users, true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(user + ";" + pass + ";" + tipo + ";" + cedula + ";" + nombres + ";" + apellidos);
+            pw.close();
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("");
+        }
+
+    }
+}
